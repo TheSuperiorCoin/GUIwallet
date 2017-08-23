231d5c2c937920818e14ab373119aed8d9703d43
mps01k <mike@asinetworkhn.com>
2017-08-23 03:13:37 -0600

    Logo on Loading

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 // Copyright (c) 2017-2020, The Superior Project// // All rights reserved.// // Redistribution and use in source and binary forms, with or without modification, are// permitted provided that the following conditions are met:// // 1. Redistributions of source code must retain the above copyright notice, this list of//    conditions and the following disclaimer.// // 2. Redistributions in binary form must reproduce the above copyright notice, this list//    of conditions and the following disclaimer in the documentation and/or other//    materials provided with the distribution.// // 3. Neither the name of the copyright holder nor the names of its contributors may be//    used to endorse or promote products derived from this software without specific//    prior written permission.// // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.//// Parts of this file are originally copyright (c) 2014-2015 The Monero Project
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
+#include <QMessageBox>
 #include <QQmlApplicationEngine>
 #include <QtQml>
 #include <QStandardPaths>
@@ -33,6 +36,8 @@
 #include "QrCodeScanner.h"
 #endif
 
+
+
 void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
     // Send all message types to logger
@@ -41,6 +46,7 @@ void messageHandler(QtMsgType type, const QMessageLogContext &context, const QSt
 
 int main(int argc, char *argv[])
 {
+
     // Enable high DPI scaling on windows & linux
 #if !defined(Q_OS_ANDROID) && QT_VERSION >= 0x050600
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -52,6 +58,27 @@ int main(int argc, char *argv[])
     qInstallMessageHandler(messageHandler);
 
     MainApp app(argc, argv);
+
+   // QPixmap pixmap("/images/Superior.png");
+    if(QPixmap(":/images/SuperiorLogo.png").isNull())
+    {
+        QMessageBox::warning(0, "Error", "Failed to load Splash Screen image!");
+    } else {
+        QSplashScreen *splash = new QSplashScreen;
+        splash->setPixmap(QPixmap(":/images/Superior.png"));
+        splash->show();
+
+        // QTimer::singleShot(2500,splash,SLOT(show()));
+         QTimer::singleShot(2500,splash,SLOT(close()));
+    }
+
+    /*
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap("images/Superior.png"));
+    splash->show();
+
+    QTimer::singleShot(2500,splash,SLOT(show()));
+*/
 
     qDebug() << "app startd";
 
